87218fe7805ac7d2134d8af841cb38569652e8ea
Dominik Kruczek <TheKiromen@gmail.com>
2021-11-11 14:28:25 +0100

    Testing FX deployment

---
+++ src/main/java/com/dkrucze/PathifyApp/Application.java
@@ -0,0 +1,23 @@
+package com.dkrucze.PathifyApp;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Application extends javafx.application.Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("MainGUI.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Pathify!");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/com/dkrucze/PathifyApp/Launcher.java
@@ -0,0 +1,8 @@
+package com.dkrucze.PathifyApp;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        Application.main(args);
+    }
+}
